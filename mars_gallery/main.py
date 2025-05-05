54316cc377b70325d55d14bd7a22dce63fd13c4a
Kuznetsov1209 <09makar12@gmail.com>
2025-05-05 17:54:18 +0300

    модернизировал приложение по задание

--- mars_gallery/main.py
+++ mars_gallery/main.py
@@ -1,21 +1,53 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/images'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+# Создаем папку для загрузок, если ее нет
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 
-@app.route('/')
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+def get_gallery_images():
+    images = []
+    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+        if allowed_file(filename):
+            images.append({
+                "filename": filename,
+                "title": "Марсианский пейзаж",
+                "description": "Фотография с поверхности Марса"
+            })
+    return images
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return redirect(url_for('carousel'))  # Перенаправление на /carousel
+    if request.method == 'POST':
+        if 'photo' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['photo']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    return redirect(url_for('carousel'))
 
 
-@app.route('/carousel')
+@app.route('/carousel', methods=['GET', 'POST'])
 def carousel():
-    images = [
-        {"filename": "mars1.jpg", "title": "Маск хочет туда перебраться"},
-        {"filename": "mars2.jpg", "title": "Маск хочет туда перебраться"},
-        {"filename": "mars3.jpg", "title": "Маск хочет туда перебраться"},
-        {"filename": "mars4.jpg", "title": "Маск хочет туда перебраться"}
-    ]
+    images = get_gallery_images()
     return render_template('gallery.html', images=images)
 
 
